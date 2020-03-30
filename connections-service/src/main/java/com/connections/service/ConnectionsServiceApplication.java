d9f6f73453d34f262bb460b92b79655fc75c3e48
jMaben <jonmaben@gmail.com>
2020-03-30 14:07:46 +0200

    Zuul implementacion

--- connections-service/src/main/java/com/connections/service/ConnectionsServiceApplication.java
+++ connections-service/src/main/java/com/connections/service/ConnectionsServiceApplication.java
@@ -3,7 +3,9 @@ package com.connections.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EntityScan({"app.commons.models.entity"})
 public class ConnectionsServiceApplication {
